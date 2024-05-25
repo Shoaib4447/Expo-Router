b6f6284656f22d34f61db369520d4ae30be21290
Shoaib4447 <125224624+Shoaib4447@users.noreply.github.com>
2024-05-25 10:05:11 +0500

    Learned Expo Router

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,23 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function _layout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+    <Tabs>
+        <Tabs.Screen name="one" options={{
+            headerTitle: "One",
+            tabBarLabel: 'One'
+        }} />
+        <Tabs.Screen name="two" options={{
+            headerTitle: "Two",
+            tabBarLabel: 'Two'
+        }} />
+        <Tabs.Screen name="posts" options={{
+            headerTitle: "Posts",
+            tabBarLabel: 'Posts',
+            headerShown: false
+        }} />
     </Tabs>
-  );
+  )
 }
